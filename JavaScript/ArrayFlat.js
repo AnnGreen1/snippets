7b4e2d5c0025822b58eba6b9d6a1489418262a29
anngreen <anngreen@qq.com>
2023-06-03 22:00:16 +0800

    array flat and unique

--- JavaScript/ArrayFlat.js
+++ JavaScript/ArrayFlat.js
@@ -10,6 +10,14 @@ const flat = (arr) =>
   );
 const flatII = (arr) =>
   arr.reduce((a, b) => (Array.isArray(b) ? [...a, ...flat(b)] : [...a, b]), []);
+
+const flat3 = (arr) => arr.flat(Infinity)
+
+const flat4 = (arr) => JSON.parse("[" + JSON.stringify(arr).replace(/\[|\]/g, "") + "]");
+
+
 console.log(flat(["cat", ["lion", "tiger"]])); // ['cat', 'lion', 'tiger']
 console.log(flat(["cat", ["lion", "tiger",["pig","monkey"]]]));
 console.log(flatII(["cat", ["lion", "tiger"]])); // ['cat', 'lion', 'tiger']
+console.log(flat3(["cat", ["lion", "tiger",["pig","monkey"]]]));
+console.log(flat4(["cat", ["lion", "tiger",["pig","monkey"]]]));
--- JavaScript/ArrayUnique.js
+++ JavaScript/ArrayUnique.js
@@ -4,6 +4,30 @@
  * @returns Array
  */
 const getUnique = (arr) => [...new Set(arr)]
+
+
+const getUnique2 = (arr) => Array.from(new Set(arr))
+
+const getUnique3 = (arr) => {
+    let newarr = [];
+    arr.forEach((item) => {
+        if(!newarr.includes(item)) newarr.push(item)
+    })
+    arr = newarr;
+    return arr;
+}
+
+const getUnique4 = (arr) => {
+    let newarr = [];
+    arr.forEach((item) => {
+        if(newarr.indexOf(item) === -1) newarr.push(item)
+    })
+    arr = newarr;
+    return arr;
+}
 // 测试
 const arr = [1, 1, 2, 3, 3, 4, 4, 5, 5];
 console.log(getUnique(arr))
+console.log(getUnique2(arr))
+console.log(getUnique3(arr))
+console.log(getUnique4(arr))
